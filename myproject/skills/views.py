c54add8269a3f34f06d1c26be9cce28bad8621ec
Ateuluz <lucas.zulueta.w@gmail.com>
2024-09-13 00:32:46 -0300

    feat(views): Define a View
    Define a simple view for displaying the list.

--- myproject/skills/views.py
+++ myproject/skills/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+# dummy data
+skills = ['Python', 'JavaScript', 'Django']
+
+def skills_list(request):
+    return render(request, 'skills/skills_list.html', {'skills': skills})
